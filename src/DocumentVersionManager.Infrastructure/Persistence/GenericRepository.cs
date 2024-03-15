6e9a4b9cf391246a7f4548d5ad766673ab0115e6
SW ENG <sweng@massload.com>
2024-03-15 15:16:02 -0600

    usless starting again

--- src/DocumentVersionManager.Infrastructure/Persistence/GenericRepository.cs
+++ src/DocumentVersionManager.Infrastructure/Persistence/GenericRepository.cs
@@ -1,8 +1,19 @@
 ﻿using DocumentVersionManager.Domain.Errors;
 using DocumentVersionManager.Domain.Interfaces;
 using DocumentVersionManager.DomainBase;
-using LanguageExt;
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+using DocumentVersionManager.DomainBase.Result;
+using DocumentVersionManager.DomainBase.Result;
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Http.HttpResults;
+=======
+=======
 >>>>>>> parent of 1e8c4a3 (added drivepath and doc name back to modelversion doc)
+=======
+>>>>>>> parent of 1e8c4a3 (added drivepath and doc name back to modelversion doc)
+using LanguageExt;
 using Microsoft.EntityFrameworkCore;
 using MySqlConnector;
 using System.Linq.Expressions;
@@ -17,7 +28,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
             _ctx = ctx;
         }
 
-        async Task<Result<GeneralFailure, int>> IGenericRepository<T>.AddAsync(T entity, CancellationToken cancellationToken)
+        async Task<Either<GeneralFailure, int>> IGenericRepository<T>.AddAsync(T entity, CancellationToken cancellationToken)
         {
             //try
             //{
@@ -56,7 +67,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
 
 
         }
-        //async Task<Result<GeneralFailure, Task<IReadOnlyList<T>>>> IGenericRepository<T>.GetAllAsync(CancellationToken cancellationToken)
+        //async Task<Either<GeneralFailure, Task<IReadOnlyList<T>>>> IGenericRepository<T>.GetAllAsync(CancellationToken cancellationToken)
         //{
 
         //    try
@@ -75,7 +86,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
         //}
 
 
-        //public async Task<Result<GeneralFailure, T>> GetMatch(Expression<Func<T, bool>> match, CancellationToken cancellationToken)
+        //public async Task<Either<GeneralFailure, T>> GetMatch(Expression<Func<T, bool>> match, CancellationToken cancellationToken)
         //{
         //    try
         //    {
@@ -91,7 +102,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
         //}
 
 
-        async Task<Result<GeneralFailure, int>> IGenericRepository<T>.UpdateAsync(T entity, CancellationToken cancellationToken)
+        async Task<Either<GeneralFailure, int>> IGenericRepository<T>.UpdateAsync(T entity, CancellationToken cancellationToken)
         {
             try
             {
@@ -113,7 +124,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
             }
 
         }
-        async Task<Result<GeneralFailure, int>> IGenericRepository<T>.DeleteAsync(T entity, CancellationToken cancellationToken)
+        async Task<Either<GeneralFailure, int>> IGenericRepository<T>.DeleteAsync(T entity, CancellationToken cancellationToken)
         {
             try
             {
@@ -131,7 +142,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
 
 
 
-        public async Task<Result<GeneralFailure, T>> GetByGuidAsync(Guid guid, CancellationToken cancellationToken)
+        public async Task<Either<GeneralFailure, T>> GetByGuidAsync(Guid guid, CancellationToken cancellationToken)
         {
             try
             {
@@ -145,7 +156,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
             }
         }
 
-        public async Task<Result<GeneralFailure, Task<IReadOnlyList<T>>>> GetAllAsyncUsingReadOnly(Expression<Func<T, bool>> expression = null, List<string> includes = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, CancellationToken cancellationToken = default)
+        public async Task<Either<GeneralFailure, Task<IReadOnlyList<T>>>> GetAllAsyncUsingReadOnly(Expression<Func<T, bool>> expression = null, List<string> includes = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, CancellationToken cancellationToken = default)
         {
             try
             {
@@ -183,7 +194,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
 
 
 
-        public async Task<Result<GeneralFailure, T>> GetMatch(Expression<Func<T, bool>> expression, List<string> includes = null, CancellationToken cancellationToken = default)
+        public async Task<Either<GeneralFailure, T>> GetMatch(Expression<Func<T, bool>> expression, List<string> includes = null, CancellationToken cancellationToken = default)
         {
             try
             {
@@ -218,7 +229,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
 
 
 
-        async Task<Result<GeneralFailure, List<T>>> IGenericRepository<T>.GetAllAsync(Expression<Func<T, bool>> expression, List<string> includes, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy, CancellationToken cancellationToken)
+        async Task<Either<GeneralFailure, List<T>>> IGenericRepository<T>.GetAllAsync(Expression<Func<T, bool>> expression, List<string> includes, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy, CancellationToken cancellationToken)
         {
             try
             {
@@ -251,8 +262,7 @@ namespace DocumentVersionManager.Infrastructure.Persistence.Repositories
                 //Log this error properly
                 return GeneralFailures.ErrorRetrievingListDataFromRepository(ex.ToString());
             }
-        }
-<<<<<<< HEAD
+
 
         }
 
